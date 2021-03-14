6c0c564cfd976aca3e9687af0eae180326e77d5a
Taeho Yoo <tyoo002@wch133-37.cs.ucr.edu>
2021-03-14 14:20:40 -0700

    fixing

--- java/compile.sh
+++ java/compile.sh
@@ -1,3 +1,9 @@
-#! /bin/bash
-rm -rf bin/*.class
+#!/bin/bash
+
+# Indicate the path of the java compiler to use
+export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Export classpath with the postgressql driver
+export CLASSPATH=$CLASSPATH:$PWD/pg73jdbc3.jar
 javac -cp ".;lib/postgresql-42.1.4.jar;" src/DBproject.java -d bin/
