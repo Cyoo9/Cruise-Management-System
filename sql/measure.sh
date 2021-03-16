265b2e4cc731e2341744c723c390775e1833564e
Caleb Yoo <calebyoo33@gmail.com>
2021-03-16 16:07:40 -0700

    Update measure.sh

--- sql/measure.sh
+++ sql/measure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-psql -h localhost -p $PGPORT $DB_NAME < create_tables.sql > /dev/null
+psql -h localhost -p $PGPORT $DB_NAME < create.sql > /dev/null
 sleep 5
 
 echo "Query time without indexes"
 cat <(echo '\timing') queries.sql | psql -h localhost -p $PGPORT $DB_NAME | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
 
-psql -h localhost -p $PGPORT $DB_NAME < create_tables.sql > /dev/null
+psql -h localhost -p $PGPORT $DB_NAME < create.sql > /dev/null
 sleep 5
 
 psql -h localhost -p $PGPORT $DB_NAME < create_indexes.sql > /dev/null
