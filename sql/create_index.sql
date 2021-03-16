ab0dea9de3ac55f4e68b3f27088a01d78b75ada8
Caleb Yoo <calebyoo33@gmail.com>
2021-03-16 16:05:43 -0700

    Update create_index.sql

--- sql/create_index.sql
+++ sql/create_index.sql
@@ -1,1 +1,14 @@
+CREATE INDEX customer_id
+ON Customer
+USING BTREE
+(id)
 
+CREATE INDEX reservation_customer_id
+ON Reservation
+USING BTREE
+(cid)
+
+CREATE INDEX reservation_status
+ON Reservation
+USING BTREE
+(status)
