e3a8896d9f36385409c877e0a87015816a75e70b
Charles <13723004+charzho@users.noreply.github.com>
2021-03-19 22:44:39 -0700

    typo

--- java/src/DBproject.java
+++ java/src/DBproject.java
@@ -615,7 +615,7 @@ public class DBproject{
 			}
 			
 			String query = "INSERT INTO Reservation (rnum, ccid, cid, status) VALUES ("
-				+ rnum + "," + ccid + "," + cid + ",'" + status + "')";
+				+ rnum + "," + ccid + "," + cnum + ",'" + status + "')";
 			esql.executeUpdate("UPDATE Cruise SET num_sold = num_sold + 1 WHERE cnum = " + cnum);
 			esql.executeQuery(query);
 			System.out.println("Reservation inserted successfully!");
